80a7f258f3ea6fb25527584287f960cd936971d9
rosebychristine <43508812+rosebychristine@users.noreply.github.com>
2020-04-05 12:05:30 +0900

    chenged db

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   mount_uploader :image, ImageUploader
   validates :title, :content, presence: true
-  has_many :images, dependent: :destroy
+  # has_many :images, dependent: :destroy
   has_many :likes, dependent: :destroy
 
   def like_user(user_id)
