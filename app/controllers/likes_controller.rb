650b83221bf421444d8d7aaee7de8d51c713f517
rosebychristine <43508812+rosebychristine@users.noreply.github.com>
2020-04-18 15:24:57 +0900

    responsive design

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,7 +5,8 @@ class LikesController < ApplicationController
     @like = Like.create(user_id: current_user.id, post_id: params[:post_id])
     like.save
     @likes = Like.where(post_id: params[:post_id])
-    @post.reload
+    @post = Food.find_by(id: @like.food_id)
+  @like_count = Like.where(food_id: params[:food_id]).count
   end
 
   def destroy
